a97dea6f815f25edb88dae17d9ced43af3a83ece
addliam <liamyostin2004@gmail.com>
2024-12-11 20:45:35 -0500

    alerta con img capibara

--- gastos/view/VhistorialGastos.php
+++ gastos/view/VhistorialGastos.php
@@ -31,82 +31,81 @@ define("BASE_URL", "http://" . $host);
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    
+
     <style>
- 
-    :root {
-        --color-primario: #5f63ad;
-        --color-secundario: #28a745;
-        --color-celeste: #16a3b8;
-        --color-verde-claro: #8ac34b;
-        --color-verde-oscuro: #1f564a;
-        --color-rojo: #dc3545;
-        --color-fondo: #fff;
-        --color-fondo-modal: rgba(0, 0, 0, 0.6);
-        --color-borde: #ddd;
-        --color-borde-hover: #e9ecef;
-        --color-scroll-thumb: #007bff;
-        --color-scroll-track: #f1f1f1;
-        --color-texto-primario: #333;
-        --color-texto-secundario: #555;
-        --color-texto-hover: #dc3545;
-    }   
+        :root {
+            --color-primario: #5f63ad;
+            --color-secundario: #28a745;
+            --color-celeste: #16a3b8;
+            --color-verde-claro: #8ac34b;
+            --color-verde-oscuro: #1f564a;
+            --color-rojo: #dc3545;
+            --color-fondo: #fff;
+            --color-fondo-modal: rgba(0, 0, 0, 0.6);
+            --color-borde: #ddd;
+            --color-borde-hover: #e9ecef;
+            --color-scroll-thumb: #007bff;
+            --color-scroll-track: #f1f1f1;
+            --color-texto-primario: #333;
+            --color-texto-secundario: #555;
+            --color-texto-hover: #dc3545;
+        }
 
-    table {
-        width: 100%;
-        border-collapse: collapse;
-        background-color: var(--color-fondo);
-        border: 2px solid var(--color-borde);
-        font-family: Arial, sans-serif;
-        font-size: 14px;
-        text-align: left;
-        margin-bottom: 20px;
-    }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            background-color: var(--color-fondo);
+            border: 2px solid var(--color-borde);
+            font-family: Arial, sans-serif;
+            font-size: 14px;
+            text-align: left;
+            margin-bottom: 20px;
+        }
 
-    th {
-        background-color: var(--color-verde-oscuro);
-        color: var(--color-fondo);
-        padding: 10px;
-        border-bottom: 3px solid var(--color-primario);
-        text-transform: uppercase;
-    }
+        th {
+            padding: 8px 28px !important;
+            text-align: center !important;
+            background-color: var(--color-verde-oscuro);
+            color: var(--color-fondo);
+            border-bottom: 3px solid var(--color-primario);
+            text-transform: uppercase;
+        }
 
-    td {
-        padding: 8px;
-        border-bottom: 1px solid var(--color-borde);
-        color: var(--color-texto-primario);
-    }
+        td {
+            padding: 8px;
+            border-bottom: 1px solid var(--color-borde);
+            color: var(--color-texto-primario);
+        }
 
-    tr:nth-child(even) {
-        background-color: var(--color-borde-hover);
-    }
+        tr:nth-child(even) {
+            background-color: var(--color-borde-hover);
+        }
 
-    tr:hover {
-        background-color: var(--color-primario);
-        color: var(--color-fondo);
-    }
+        tr:hover {
+            background-color: var(--color-primario);
+            color: var(--color-fondo);
+        }
 
-    .btn-delete {
-        background-color: var(--color-rojo);
-        color: var(--color-fondo);
-        border: none;
-        padding: 5px 10px;
-        border-radius: 5px;
-        cursor: pointer;
-    }
+        .btn-delete {
+            background-color: var(--color-rojo);
+            color: var(--color-fondo);
+            border: none;
+            padding: 5px 10px;
+            border-radius: 5px;
+            cursor: pointer;
+        }
 
-    .btn-delete:hover {
-        background-color: var(--color-texto-hover);
-        opacity: 0.9;
-    }
+        .btn-delete:hover {
+            background-color: var(--color-texto-hover);
+            opacity: 0.9;
+        }
 
-    .table-header {
-        background-color: var(--color-primario);
-        color: var(--color-fondo);
-        font-weight: bold;
-    }
-
-</style>
+        .table-header {
+            background-color: var(--color-primario);
+            color: var(--color-fondo);
+            font-weight: bold;
+        }
+    </style>
 
 </head>
 
@@ -122,7 +121,7 @@ define("BASE_URL", "http://" . $host);
         <div class="table-responsive">
             <div class="table-container">
                 <table class="table table-bordered table-sm">
-                    <thead >
+                    <thead>
                         <tr class="tr-cabecera">
                             <th>ID Gasto</th>
                             <th>Categoría</th>
@@ -204,7 +203,7 @@ define("BASE_URL", "http://" . $host);
                 });
                 // showModal(result.message);
                 // Recargar para actualizar vista de tabla
-                setTimeout(function() {
+                setTimeout(function () {
                     location.reload();
                 }, 500);
             });
@@ -213,67 +212,77 @@ define("BASE_URL", "http://" . $host);
         document.addEventListener("DOMContentLoaded", async () => {
             // const response = await fetch(`${BASE_URL}/gerenciaweb-main/controller/GastoControlador.php?op=obtenerFechaUltimoGasto`, {
             const response = await fetch(`../controller/GastoControlador.php?op=obtenerFechaUltimoGasto`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ usuario_id })
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ usuario_id })
             })
 
             const result = await response.json();
-console.log(result.data)
-        const lastGastoDate = new Date(result.data);
-        const currentDate = new Date();
-        const differenceInDays = Math.floor((currentDate - lastGastoDate) / (1000 * 60 * 60 * 24));
-console.log(lastGastoDate)
-        if (differenceInDays >= 7) {
-            showAnimatedNotification(`¡Hace ${differenceInDays - 1 } días que no registras un gasto!`);
-            // console.log(`¡Hace ${differenceInDays} días que no registras un gasto!`);
+            console.log(result.data)
+            const lastGastoDate = new Date(result.data);
+            const currentDate = new Date();
+            const differenceInDays = Math.floor((currentDate - lastGastoDate) / (1000 * 60 * 60 * 24));
+            console.log(lastGastoDate)
+            if (differenceInDays >= 7) {
+                showAnimatedNotification(`¡Hace ${differenceInDays - 1} días que no registras un gasto!`);
+                // console.log(`¡Hace ${differenceInDays} días que no registras un gasto!`);
 
-        }
+            }
 
             // localStorage.setItem("lastAccessDate", currentDate.toISOString());
             console.log(result);
 
-            function showAnimatedNotification(message) {
-            const notificationContainer = document.createElement("div");
-            notificationContainer.style.position = "fixed";
-            notificationContainer.style.top = "20px";
-            notificationContainer.style.right = "20px";
-            notificationContainer.style.padding = "20px";
-            notificationContainer.style.backgroundColor = "#FFD700";
-            notificationContainer.style.color = "#000";
-            notificationContainer.style.fontSize = "16px";
-            notificationContainer.style.fontWeight = "bold";
-            notificationContainer.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.2)";
-            notificationContainer.style.borderRadius = "8px";
-            notificationContainer.style.transition = "transform 0.5s ease, opacity 0.5s ease";
-            notificationContainer.style.zIndex = "1000";
+            function showAnimatedNotification(message, imageSource) {
+                const notificationContainer = document.createElement("div");
+                // imagen capibara
+                const img = document.createElement("img");
+                img.src = imageSource ? imageSource : "/gerenciaweb/icons/capibara/capybara_indiferente.png";
+                img.alt = "Imagen capibara";
+                img.style.width = "100px";
+                img.style.height = "100px";
+                console.log(notificationContainer);
+                // sgte
+                notificationContainer.style.position = "fixed";
+                notificationContainer.style.display = "block";
+                notificationContainer.style.top = "20px";
+                notificationContainer.style.right = "20px";
+                notificationContainer.style.padding = "20px";
+                notificationContainer.style.backgroundColor = "#FFD700";
+                notificationContainer.style.color = "#000";
+                notificationContainer.style.fontSize = "16px";
+                notificationContainer.style.fontWeight = "bold";
+                notificationContainer.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.2)";
+                notificationContainer.style.borderRadius = "8px";
+                notificationContainer.style.transition = "transform 0.5s ease, opacity 0.5s ease";
+                notificationContainer.style.zIndex = "1000";
 
-            notificationContainer.textContent = message;
-            document.body.appendChild(notificationContainer);
+                notificationContainer.textContent = message;
+                document.body.appendChild(notificationContainer);
+                notificationContainer.appendChild(img);
 
-            // Animación de entrada
-            notificationContainer.style.transform = "translateX(100%)";
-            notificationContainer.style.opacity = "0";
-            setTimeout(() => {
-                notificationContainer.style.transform = "translateX(0)";
-                notificationContainer.style.opacity = "1";
-            }, 100);
-
-            // Desaparición después de 5 segundos
-            setTimeout(() => {
+                // Animación de entrada
                 notificationContainer.style.transform = "translateX(100%)";
                 notificationContainer.style.opacity = "0";
                 setTimeout(() => {
-                    notificationContainer.remove();
-                }, 500);
-            }, 5000);
-        }
+                    notificationContainer.style.transform = "translateX(0)";
+                    notificationContainer.style.opacity = "1";
+                }, 100);
 
-});
+                // Desaparición después de 5 segundos
+                setTimeout(() => {
+                    notificationContainer.style.transform = "translateX(100%)";
+                    notificationContainer.style.opacity = "0";
+                    setTimeout(() => {
+                        notificationContainer.remove();
+                    }, 500);
+                }, 5000);
+            }
+
+        });
 
 
 
-       
+
 
     </script>
 
